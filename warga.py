6a3838d6ee07a8d202a87d31629d1b089b05aebf
unknown <sucisry@gmail.com>
2024-04-15 07:45:30 +0700

    fea: update

---
+++ warga.py
@@ -0,0 +1,7 @@
+class Warga():
+    def __init__(self, NIK):
+        self.nik = NIK
+
+    def nyoblos(self, pilihan):
+        self.pilihan = pilihan
+        print(f'{self.nik} memilih {pilihan}')
